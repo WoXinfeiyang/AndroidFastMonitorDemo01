a8443494989aa60034893253b9663073db2a1c11
andyfu <815965325@qq.com>
2021-04-14 23:57:22 +0800

    temp commit

--- fastMonitorPlugins/src/main/kotlin/com/fxj/fastMonitorPlugins/convertToWebpPlugin/processor/ConvertToWebpVariantProcessor.kt
+++ fastMonitorPlugins/src/main/kotlin/com/fxj/fastMonitorPlugins/convertToWebpPlugin/processor/ConvertToWebpVariantProcessor.kt
@@ -1,7 +1,12 @@
 package com.fxj.fastMonitorPlugins.convertToWebpPlugin.processor
 
+import com.android.build.api.variant.impl.ApplicationVariantImpl
 import com.android.build.gradle.api.BaseVariant
+import com.android.build.gradle.internal.variant.ApkVariantData
+import com.android.build.gradle.tasks.MergeResources
+import com.fxj.fastMonitorPlugins.convertToWebpPlugin.task.ConvertToWebpTask
 import com.google.auto.service.AutoService
+import org.gradle.api.Task
 
 @AutoService(VariantProcessor::class)
 class ConvertToWebpVariantProcessor:VariantProcessor {
@@ -10,6 +15,16 @@ class ConvertToWebpVariantProcessor:VariantProcessor {
     }
     override fun process(variant: BaseVariant) {
         System.out.println(TAG+": ##process##variant=${variant}")
+        var variantData: ApkVariantData =(variant as com.android.build.gradle.internal.api.ApplicationVariantImpl).variantData
+
+        var tasks=variantData.getScope().getGlobalScope().getProject().getTasks()
+
+        var convertToWebpTask: Task = tasks.findByName("ConvertToWebpTask")?:tasks.create(
+            "ConvertToWebpTask",
+            ConvertToWebpTask::class.java)
+
+        var mergeResources: MergeResources= variant.getMergeResourcesProvider().get()
+        mergeResources.dependsOn(convertToWebpTask)
     }
 
 }
